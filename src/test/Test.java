f129325e5acf410ff02f4272b41cd720e3775b81
archanjojr <Math.calazans56@gmail.com>
2017-11-29 21:21:58 -0200

    add view files and functions, in conjuction with test's

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,18 +1,12 @@
 package test;
 
 import model.DataManager;
-
+import view.View;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DataManager mDataManager = new DataManager();
-		mDataManager.getInstance();
-		
-		mDataManager.readFile();
-		
-		System.out.println(mDataManager.words.getItemCount());
-		
+		View v =new View();
+		v.PrintTerm();
 	}
 
 }
